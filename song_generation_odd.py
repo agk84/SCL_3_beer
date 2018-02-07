040b7358279db0fd96b0b9b311837169a87757bd
hj424 <hj424@cornell.edu>
2018-02-07 16:13:37 -0500

    add

--- song_generation_odd.py
+++ song_generation_odd.py
@@ -1,3 +1,3 @@
 #
-for i in range(0,100,2):
-  print(str(100-i)+"bottles of beer on the wall, take one down, pass it around,"+str(99-1)+"bottles of beer on the wall.")
+for i in range(0,89,2):
+  print(str(89-i)+"bottles of beer on the wall, take one down, pass it around,"+str(88-i)+"bottles of beer on the wall.")
